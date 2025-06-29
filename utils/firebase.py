70611c485f57a1dc658937bc7080bb30242cbfb3
brandonvanderventel <brandonvanderventel@gmail.com>
2025-06-29 22:28:18 +0000

    Correct PDF 00H28 30062025

--- utils/firebase.py
+++ utils/firebase.py
@@ -22,7 +22,7 @@ if not firebase_admin._apps:
     # })
 
     firebase_admin.initialize_app(cred, {
-    "storageBucket": digitalproducts-97155.appspot.com
+    "storageBucket": "digitalproducts-97155.firebasestorage.app"
 })
 
 def check_if_pdf_exists(book: str) -> str | None:
